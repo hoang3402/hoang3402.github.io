6dafa088acf36477c9cda9e6bd7c67b93d7c0ca5
HoangDSR <nvh.02021995@gmail.com>
2023-05-24 23:31:38 +0700

    Fix nav active, update jquery

--- js/main.js
+++ js/main.js
@@ -10,33 +10,12 @@
 'use strict';
 
 (function ($) {
-
     /*------------------
         Preloader
     --------------------*/
     $(window).on('load', function () {
         $(".loader").fadeOut();
         $("#preloder").delay(200).fadeOut("slow");
-
-        /*------------------
-            FIlter
-        --------------------*/
-        $('.filter__controls li').on('click', function () {
-            $('.filter__controls li').removeClass('active');
-            $(this).addClass('active');
-        });
-        if ($('.filter__gallery').length > 0) {
-            var containerEl = document.querySelector('.filter__gallery');
-            var mixer = mixitup(containerEl);
-        }
-    });
-
-    /*------------------
-		Navigation
-	--------------------*/
-    $('.mobile-menu').slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true,
     });
 
     /*------------------
@@ -51,46 +30,52 @@
         Niceselect
     --------------------*/
     $('select').niceSelect();
+
+    /*------------------
+        Angular
+    --------------------*/
+
+    angular.module('myApp', ['ngRoute'])
+        .component('productItem', {
+            templateUrl: '../components/product_item/product_item.html',
+            bindings: {
+                name: '@',
+                url: '@',
+                comment: '@',
+                views: '@',
+                genres: '@'
+            },
+        }).component('headerCustoms', {
+            templateUrl: '../components/header/header.html',
+            bindings: {
+                name: '@',
+                url: '@',
+            },
+        }).component('heroSlider', {
+            templateUrl: '../components/hero_slider/hero_slider.html',
+            bindings: {
+                name: '@',
+                url: '@',
+            },
+        }).component('productSection', {
+            templateUrl: '../components/product_section/product_section.html'
+        }).component('footerCustoms', {
+            templateUrl: '../components/footer/fotter.html'
+        }).component('search', {
+            templateUrl: '../components/search/search.html'
+        }).config(function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/login', {
+                    templateUrl: '../views/login.html',
+                    controller: '',
+                }).when('/signup', {
+                    templateUrl: '../views/signup.html',
+                    controller: '',
+                }).when('/blog', {
+                    templateUrl: '../views/blog.html'
+                })
+                .otherwise({
+                    template: '<hero-slider></hero-slider> <product-section></product-section>'
+                });
+        });
 })(jQuery);
-
-// Angular
-
-angular.module('myApp', ['ngRoute'])
-    .component('productItem', {
-        templateUrl: '../components/product_item/product_item.html',
-        bindings: {
-            name: '@',
-            url: '@',
-            comment: '@',
-            views: '@',
-            genres: '@'
-        },
-    }).component('headerCustoms', {
-        templateUrl: '../components/header/header.html',
-        bindings: {
-            name: '@',
-            url: '@',
-        },
-    }).component('heroSlider', {
-        templateUrl: '../components/hero_slider/hero_slider.html',
-        bindings: {
-            name: '@',
-            url: '@',
-        },
-    }).component('productSection', {
-        templateUrl: '../components/product_section/product_section.html'
-    }).component('footerCustoms', {
-        templateUrl: '../components/footer/fotter.html'
-    }).component('search', {
-        templateUrl: '../components/search/search.html'
-    }).config(function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/login', {
-                templateUrl: '../views/login.html'
-            }).when('/signup', {
-                templateUrl: '../views/signup.html'
-            })
-            .otherwise({
-                template: '<hero-slider></hero-slider> <product-section></product-section>'
-            });
-    });
