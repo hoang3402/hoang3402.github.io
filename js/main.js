6e61e971e0bcabe099714de462caf3bf6e93d71e
HoangDSR <nvh.02021995@gmail.com>
2023-05-25 19:32:47 +0700

    Fix link footer,

--- js/main.js
+++ js/main.js
@@ -29,71 +29,80 @@
         Angular
     --------------------*/
 
-    angular.module('myApp', ['ngRoute'])
-        .component('productItem', {
-            templateUrl: '../components/product_section/product_item.html',
-            bindings: {
-                name: '@',
-                url: '@',
-                comment: '@',
-                views: '@',
-                genres: '@'
-            },
-        }).component('headerCustoms', {
-            templateUrl: '../components/header/header.html',
-            bindings: {
-                name: '@',
-                url: '@',
-            },
-        }).component('heroSlider', {
-            templateUrl: '../components/hero_slider/hero_slider.html',
-            bindings: {
-                name: '@',
-                url: '@',
-            },
-        }).component('productSection', {
-            templateUrl: '../components/product_section/product_section.html'
-        }).component('footerCustoms', {
-            templateUrl: '../components/footer/fotter.html'
-        }).component('search', {
-            templateUrl: '../components/search/search.html'
-        }).component('breadcrumb', {
-            templateUrl: '../components/breadcrumb/breadcrumb.html'
-        }).component('heroItem', {
-            templateUrl: '../components/hero_slider/hero_items.html',
-            bindings: {
-                idMovie: '@',
-                name: '@',
-                linkImage: '@',
-                description: '@',
-            }
-        }).config(function ($routeProvider) {
-            $routeProvider
-                .when('/login', {
-                    templateUrl: '../views/login.html',
-                    controller: '',
-                })
-                .when('/signup', {
-                    templateUrl: '../views/signup.html',
-                    controller: '',
-                })
-                .when('/blog', {
-                    templateUrl: '../views/blog.html'
-                })
-                .when('/categories', {
-                    templateUrl: '../views/categories.html'
-                })
-                .when('/anime-details', {
-                    templateUrl: '../views/anime-details.html'
-                })
-                .when('/anime-watching', {
-                    templateUrl: '../views/anime-watching.html'
-                })
-                .when('/blog-details', {
-                    templateUrl: '../views/blog-details.html'
-                })
-                .otherwise({
-                    template: '<hero-slider></hero-slider> <product-section></product-section>',
-                });
-        });
+    var app = angular.module('myApp', ['ngRoute']);
+
+    app.component('productItem', {
+        templateUrl: '../components/product_section/product_item.html',
+        bindings: {
+            name: '@',
+            url: '@',
+            comment: '@',
+            views: '@',
+            genres: '@'
+        },
+    })
+    app.component('headerCustoms', {
+        templateUrl: '../components/header/header.html',
+        bindings: {
+            name: '@',
+            url: '@',
+        },
+    })
+    app.component('heroSlider', {
+        templateUrl: '../components/hero_slider/hero_slider.html',
+        bindings: {
+            name: '@',
+            url: '@',
+        },
+    })
+    app.component('productSection', {
+        templateUrl: '../components/product_section/product_section.html'
+    })
+    app.component('footerCustoms', {
+        templateUrl: '../components/footer/footer.html'
+    })
+    app.component('search', {
+        templateUrl: '../components/search/search.html'
+    })
+    app.component('breadcrumb', {
+        templateUrl: '../components/breadcrumb/breadcrumb.html'
+    })
+    app.component('heroItem', {
+        templateUrl: '../components/hero_slider/hero_items.html',
+        bindings: {
+            idMovie: '@',
+            name: '@',
+            linkImage: '@',
+            description: '@',
+        }
+    })
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: '../views/login.html',
+                controller: '',
+            })
+            .when('/signup', {
+                templateUrl: '../views/signup.html',
+                controller: '',
+            })
+            .when('/blog', {
+                templateUrl: '../views/blog.html'
+            })
+            .when('/categories', {
+                templateUrl: '../views/categories.html'
+            })
+            .when('/anime-details', {
+                templateUrl: '../views/anime-details.html'
+            })
+            .when('/anime-watching', {
+                templateUrl: '../views/anime-watching.html'
+            })
+            .when('/blog-details', {
+                templateUrl: '../views/blog-details.html'
+            })
+            .otherwise({
+                template: '<hero-slider></hero-slider> <product-section></product-section>',
+            });
+    });
 })(jQuery);
