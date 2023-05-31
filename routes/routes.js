70bdab0be7ac6ae6a6302f1fd9094fc6c46d3998
HoangDSR <nvh.02021995@gmail.com>
2023-05-31 21:37:22 +0700

    clean code

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,37 @@
 app.config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/login', {
-            templateUrl: '../views/login.html',
-            controller: '',
-        })
-        .when('/signup', {
-            templateUrl: '../views/signup.html',
-            controller: '',
-        })
-        .when('/blog', {
-            templateUrl: '../views/blog.html'
-        })
-        .when('/categories', {
-            templateUrl: '../views/categories.html'
-        })
-        .when('/anime-details/:id', {
-            templateUrl: '../views/anime-details.html',
-            // controller: 'AnimeDetailsController',
-        })
-        .when('/anime/:animeId/episode/:id', {
-            templateUrl: '../views/anime-watching.html'
-        })
-        .when('/blog-details', {
-            templateUrl: '../views/blog-details.html',
-            controller: 'BlogDetail'
-        })
-        .otherwise({
-            template: '<hero-slider></hero-slider> <product-section></product-section>',
-        });
+    $routeProvider.when('/login', {
+        templateUrl: '../views/login.html',
+        controller: '',
+    })
+
+    $routeProvider.when('/signup', {
+        templateUrl: '../views/signup.html',
+        controller: '',
+    })
+
+    $routeProvider.when('/blog', {
+        templateUrl: '../views/blog.html'
+    })
+
+    $routeProvider.when('/categories', {
+        templateUrl: '../views/categories.html'
+    })
+
+    $routeProvider.when('/anime-details/:id', {
+        templateUrl: '../views/anime-details.html',
+        // controller: 'AnimeDetailsController',
+    })
+
+    $routeProvider.when('/anime/:animeId/episode/:id', {
+        templateUrl: '../views/anime-watching.html'
+    })
+
+    $routeProvider.when('/blog-details', {
+        templateUrl: '../views/blog-details.html',
+        controller: 'BlogDetail'
+    })
+
+    $routeProvider.otherwise({
+        template: '<hero-slider></hero-slider> <product-section></product-section>',
+    });
 });
