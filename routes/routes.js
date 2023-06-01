ba4a2423fd9a3d79d2530399d90d5dafe87f9147
HoangDSR <nvh.02021995@gmail.com>
2023-06-01 21:56:39 +0700

    remove unused

--- routes/routes.js
+++ routes/routes.js
@@ -1,37 +1,36 @@
-app.config(function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/login', {
-        templateUrl: '../views/login.html',
-        controller: '',
-    })
+app.config(function ($routeProvider) {
+	$routeProvider.when('/login', {
+		templateUrl: '../views/login.html',
+		controller: '',
+	});
 
-    $routeProvider.when('/signup', {
-        templateUrl: '../views/signup.html',
-        controller: '',
-    })
+	$routeProvider.when('/signup', {
+		templateUrl: '../views/signup.html',
+		controller: '',
+	});
 
-    $routeProvider.when('/blog', {
-        templateUrl: '../views/blog.html'
-    })
+	$routeProvider.when('/blog', {
+		templateUrl: '../views/blog.html',
+	});
 
-    $routeProvider.when('/categories', {
-        templateUrl: '../views/categories.html'
-    })
+	$routeProvider.when('/categories', {
+		templateUrl: '../views/categories.html',
+	});
 
-    $routeProvider.when('/anime-details/:id', {
-        templateUrl: '../views/anime-details.html',
-        // controller: 'AnimeDetailsController',
-    })
+	$routeProvider.when('/anime-details/:id', {
+		templateUrl: '../views/anime-details.html',
+	});
 
-    $routeProvider.when('/anime/:animeId/episode/:id', {
-        templateUrl: '../views/anime-watching.html'
-    })
+	$routeProvider.when('/anime/:animeId/episode/:id', {
+		templateUrl: '../views/anime-watching.html',
+	});
 
-    $routeProvider.when('/blog-details', {
-        templateUrl: '../views/blog-details.html',
-        controller: 'BlogDetail'
-    })
+	$routeProvider.when('/blog-details', {
+		templateUrl: '../views/blog-details.html',
+		controller: 'BlogDetail',
+	});
 
-    $routeProvider.otherwise({
-        template: '<hero-slider></hero-slider> <product-section></product-section>',
-    });
+	$routeProvider.otherwise({
+		template: '<hero-slider></hero-slider> <product-section></product-section>',
+	});
 });
