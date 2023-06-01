88e050e4f41e5ed92e25faad772200b8d8252170
HoangDSR <nvh.02021995@gmail.com>
2023-06-01 15:43:18 +0700

    done

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,61 +1,76 @@
 const DOMAIN = 'https://hoang3409.alwaysdata.net/index.php';
 
-app.controller("GetListAnimeTrending", function ($scope, $http) {
-    $http({
-            method: "GET",
-            url: `${DOMAIN}/Anime/GetListAnimeTrending/6`,
-        })
-        .then((res) => {
-            $scope.list = res.data;
-            console.log(`.then ~ res.data:`, res.data)
-        }, () => {
-            alert("Server bị gì rồi á");
-        })
-})
+app.controller('GetListAnimeTrending', function ($scope, $http) {
+	$http({
+		method: 'GET',
+		url: `${DOMAIN}/Anime/GetListAnimeTrending/6`,
+	}).then(
+		(res) => {
+			$scope.list = res.data;
+			console.log(`.then ~ res.data:`, res.data);
+		},
+		() => {
+			alert('Server bị gì rồi á');
+		},
+	);
+});
 
-app.controller("GetGenres", function ($scope, $http) {
-    $http({
-            method: "GET",
-            url: `${DOMAIN}/Anime/GetGenres`
-        })
-        .then((res) => {
-            $scope.genres = res.data;
-        }, (res) => {
-            console.log("Failed: ", res.data)
-        })
-})
+app.controller('GetGenres', function ($scope, $http) {
+	$http({
+		method: 'GET',
+		url: `${DOMAIN}/Anime/GetGenres`,
+	}).then(
+		(res) => {
+			$scope.genres = res.data;
+		},
+		(res) => {
+			console.log('Failed: ', res.data);
+		},
+	);
+});
 
-app.controller("BlogDetail", function ($anchorScroll) {
-    $anchorScroll();
-})
+app.controller('BlogDetail', function ($anchorScroll) {
+	$anchorScroll();
+});
 
-app.controller("AnimeDetailsController", function ($scope, $routeParams, $anchorScroll, $http) {
-    $anchorScroll();
-    $scope.id = $routeParams.id;
+app.controller(
+	'AnimeDetailsController',
+	function ($scope, $routeParams, $anchorScroll, $http) {
+		$anchorScroll();
+		$scope.id = $routeParams.id;
+		$scope.showFullText = false;
 
-    $http({
-            method: "GET",
-            url: `${DOMAIN}/Anime/GetAnimeById/${$scope.id}`
-        })
-        .then((res) => {
-            var data = res.data[0];
-            $scope.title = data.title
-            $scope.linkImage = data.cover_image_url
-            $scope.description = data.description
-            $scope.views = data.views
-            $scope.genres = data.genres
-        })
-})
+		$scope.toggleShowFullText = function (event) {
+			event.preventDefault();
+			$scope.showFullText = !$scope.showFullText;
+		};
 
-app.controller("AnimeWatchingController", function ($scope, $routeParams, $anchorScroll, $http) {
-    $anchorScroll();
-    $scope.animeId = $routeParams.animeId;
-    $http({
-            method: "GET",
-            url: `${DOMAIN}/Anime/GetMovies/${$scope.animeId}`
-        })
-        .then((res) => {
-            $scope.data = res.data;
-            $scope.url = res.data[$routeParams.id - 1].video_url;
-        })
-})
+		$http({
+			method: 'GET',
+			url: `${DOMAIN}/Anime/GetAnimeById/${$scope.id}`,
+		}).then((res) => {
+			var data = res.data[0];
+			$scope.title = data.title;
+			$scope.linkImage = data.cover_image_url;
+			$scope.descriptionShort = data.description.substring(0, 250);
+			$scope.description = data.description;
+			$scope.views = data.views;
+			$scope.genres = data.genres;
+		});
+	},
+);
+
+app.controller(
+	'AnimeWatchingController',
+	function ($scope, $routeParams, $anchorScroll, $http) {
+		$anchorScroll();
+		$scope.animeId = $routeParams.animeId;
+		$http({
+			method: 'GET',
+			url: `${DOMAIN}/Anime/GetMovies/${$scope.animeId}`,
+		}).then((res) => {
+			$scope.data = res.data;
+			$scope.url = res.data[$routeParams.id - 1].video_url;
+		});
+	},
+);
