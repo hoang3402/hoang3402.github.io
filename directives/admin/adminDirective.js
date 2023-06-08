92efd17cf1693bec48e717e7604ba1a3b4158c6e
HoangDSR <nvh.02021995@gmail.com>
2023-06-08 20:17:54 +0700

    Done datatable for admin

--- directives/admin/adminDirective.js
+++ directives/admin/adminDirective.js
@@ -1,5 +1,7 @@
 var app = angular.module('myApp', ['ngRoute']);
 
+const DOMAIN = 'https://hoang3409.alwaysdata.net/index.php/Admin/';
+
 app.directive('navbar', () => {
 	return {
 		restrict: 'E',
@@ -20,12 +22,52 @@ app.directive('sideBar', () => {
 	};
 });
 
-app.directive('datatable', function () {
+app.directive('datatable', function ($routeParams) {
 	return {
 		restrict: 'E',
 		templateUrl: './table/table.html',
-		controller: () => {
-			console.log('load dataTable');
+		controller: ($scope, $http) => {
+			$scope.nameTable = $routeParams.name;
+			$http({
+				method: 'GET',
+				url: DOMAIN + $routeParams.name,
+			}).then((res) => {
+				$scope.data = res.data;
+
+				var theadRow = angular.element('#example1 thead tr');
+				var tfootRow = angular.element('#example1 tfoot tr');
+				angular.forEach($scope.data[0], function (value, key) {
+					var th = angular.element('<th>').text(key);
+					var clonedTh = th.clone();
+					tfootRow.append(th);
+					theadRow.append(clonedTh);
+				});
+
+				var tbody = angular.element('#example1 tbody');
+				angular.forEach($scope.data, function (item) {
+					var tr = angular.element('<tr>');
+					angular.forEach(item, function (value, key) {
+						var td = angular.element('<td>').text(value);
+						tr.append(td);
+					});
+					tbody.append(tr);
+				});
+
+				$scope.setupTable();
+			});
+
+			$scope.setupTable = () => {
+				$('#example1')
+					.DataTable({
+						responsive: true,
+						lengthChange: false,
+						autoWidth: false,
+						buttons: ['copy', 'csv', 'excel', 'pdf', 'print', 'colvis'],
+					})
+					.buttons()
+					.container()
+					.appendTo('#example1_wrapper .col-md-6:eq(0)');
+			};
 		},
 	};
 });
