c34468977fb147bc5a5b16774a43a0ab01349836
HoangDSR <nvh.02021995@gmail.com>
2023-06-11 22:09:33 +0700

    center text

--- directives/admin/route.js
+++ directives/admin/route.js
@@ -9,6 +9,6 @@ app.config(function ($routeProvider) {
 		template: '<create></create>',
 	});
 	$routeProvider.otherwise({
-		template: '<h1>Hello Admin</h1>',
+		template: '<h1 class="text-center">Hello Admin</h1>',
 	});
 });
